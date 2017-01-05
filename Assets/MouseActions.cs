f6a368dfe881661049bcc3eaaa7798f94f2be6b2
aserra92 <aria.serrafelip@gmail.com>
2017-01-05 18:58:46 +0100

    Teleport done

--- Assets/MouseActions.cs
+++ Assets/MouseActions.cs
@@ -8,28 +8,30 @@ using System.Threading;
 
 public class MouseActions : MonoBehaviour {
 
-    private TerrainSystem ts;
-    // Use this for initialization
-    void Start () {
-        ts = TerrainSystem.TS;
-    }
+	private TerrainSystem ts;
+	// Use this for initialization
+	void Start () {
+		ts = TerrainSystem.TS;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButton(0))
-        { 
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            
-            if (Physics.Raycast(ray, out hit, 35f))
-            {
-                //Debug.LogWarning(hit.transform.gameObject.name);
-                Chunk tempChunk = ts.findChunk(hit.transform.gameObject.name);
-                
-                tempChunk.updateMesh(hit.point);
-                
-            }
+		if (Input.GetMouseButton(0))
+		{ 
+			RaycastHit hit;
+			if (Camera.main != null)
+			{
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        }
-    }
+				if (Physics.Raycast(ray, out hit, 35f))
+				{
+					//Debug.LogWarning(hit.transform.gameObject.name);
+					Chunk tempChunk = ts.findChunk(hit.transform.gameObject.name);
+
+					tempChunk.updateMesh(hit.point);
+
+				}
+			}
+		}
+	}
 }
---
+++ Assets/Scripts/CharacterSystem/Skin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skin {
+    /*
+    HatSkin hat;
+    TopSkin top;
+    ArmSkin arm;
+    WeaponSkin weapon;
+    PantsSkin pants;
+    ShoesSkin shoes;
+
+    */
+
+}
