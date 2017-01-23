491035279257a86d690513f1e8fee570b6e0f0b5
carlos <carloscbl@hotmail.com>
2017-01-23 01:56:22 +0100

    Projectile system, and projectile base

---
+++ Assets/Scripts/Battle/Projectiles/ProjectileSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+
+class ProjectileSystem : MonoBehaviour
+{
+    private uint MaxProjectilesPoolSize = 128;
+    private List<GameObject> projectilesList;
+    private List<GameObject> freeProjectilesList;
+    private void Start()
+    {
+        //instantiate the pool;
+        instantiatePoolOfProjectiles();
+    }
+
+    private void instantiatePoolOfProjectiles()
+    {
+        projectilesList = new List<GameObject>();
+        for (int i = 0; i < MaxProjectilesPoolSize; i++)
+        {
+            projectilesList.Add(GameObject.Instantiate((Resources.Load("Projectiles/ProjectileBase")) as GameObject,this.transform));
+            freeProjectilesList = new List<GameObject>(projectilesList);
+        }
+    } 
+}
+
