04a9ac1ac702731ee139f591d26d1b221c95ca59
carlos <carloscbl@hotmail.com>
2017-02-03 00:43:25 +0100

    no message

--- Assets/Scripts/Enviroment/Parallax.cs
+++ Assets/Scripts/Enviroment/Parallax.cs
@@ -13,14 +13,16 @@ public class Parallax : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         OriginalPosition = this.transform.position;
-        last = GameObject.Find("CharacterSystem").GetComponent<CharacterSystem>().mainCharacter.transform.position.y;
+        lastY = GameObject.Find("CharacterSystem").GetComponent<CharacterSystem>().mainCharacter.transform.position.y;
+        lastX = GameObject.Find("CharacterSystem").GetComponent<CharacterSystem>().mainCharacter.transform.position.x;
     }
     
     public Vector2 Max = new Vector2(150,150);
     private Vector2 OriginalPosition;
     float currentY = 0;
     float DestinoY = 0,currSpeed = 0;
-    float last;
+    float lastY;
+    float lastX;
     int count = 0;
     // Update is called once per frame
     void Update () {
@@ -34,30 +36,49 @@ public class Parallax : MonoBehaviour {
                 this.transform.position = new Vector3(tempPos.x, tempPos.y, 0.5f);
                 break;
             case ParallaxLayer.Middle:
-
-                
-                
-                Vector3 tempPos2 = Player.GetComponent<Character>().myCamera.transform.position;
-                Vector2 currentPos = new Vector2(this.transform.position.x, DestinoY);
-                Vector2 maxPos = OriginalPosition + Max;
-                Vector2 minPos = OriginalPosition - Max;
-                if(count == 0)
-                {
-                    last = this.transform.position.y;
-                    count = 1;
-                }
-                //this.transform.Translate(new Vector2(0, this.transform.position.y - (Player.transform.position.y - last)) );//* 400 / 500)
-                this.transform.Translate(new Vector2(0,  (Player.transform.position.y - last) * 400 / 500));//)
-                print(Player.transform.position.y -last);
-                last = Player.transform.position.y;
-                print(last);
-                
-
+                Middle();
                 break;
             case ParallaxLayer.Close:
+                Close();
                 break;
             default:
                 break;
         }
+    }
+    public float closeY = 100;
+    public float closeX = 100;
+    public float middleY = 250;
+    public float middleX = 250;
+    void Close()
+    {
+        Translation(closeY, closeX,true);
+    }
+
+    void Translation(float maxY, float maxX ,bool Close)
+    {
+        if (count == 0)
+        {
+            lastY = this.transform.position.y;
+            lastX = this.transform.position.x;
+            count = 1;
+        }
+        if (Close)
+        {
+            this.transform.Translate(new Vector2(Player.transform.position.x - lastX, (Player.transform.position.y - lastY) * maxY / 500));
+            lastY = Player.transform.position.y;
+            lastX = Player.transform.position.x;
+            //float interp = Mathf.Lerp(0, 50, );
+            this.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(Mathf.Clamp(Player.transform.position.x, -250, 12000) / 64, 0 ));
+        }else
+        {
+            this.transform.Translate(new Vector2((Player.transform.position.x - lastX) * maxX / 500, (Player.transform.position.y - lastY) * maxY / 500));
+            lastY = Player.transform.position.y;
+            lastX = Player.transform.position.x;
+        }
+        
+    }
+    void Middle()
+    {
+        Translation(middleY, middleX,false);
     }
 }
