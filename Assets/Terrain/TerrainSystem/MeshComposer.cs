8e1004b25d414723a54ffaff245a116f9f223f2f
carlos <carloscbl@hotmail.com>
2017-02-18 19:21:33 +0100

    Starting the refactor

---
+++ Assets/Terrain/TerrainSystem/MeshComposer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+class MeshComposer
+{
+    byte[] data;
+    List<Vector3[]> vertexList      = new List<Vector3[]>();
+    List<Vector3[]> normalsList     = new List<Vector3[]>();
+    List<Vector3[]> uvsList         = new List<Vector3[]>();
+    List<int[]>     trianglesList   = new List<int[]>();
+    Hashtable hashTable = new Hashtable(255);
+    List<List<int>> positionsInChunk = new List<List<int>>();
+    public MeshComposer(byte [] data)
+    {
+        this.data = data;
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (hashTable.ContainsKey(data[i]))
+            {
+               List<int> temp = hashTable[data[i]] as List<int>;
+                temp.Add(i);
+            }else
+            {
+                List<int> temp = new List<int>();
+                temp.Add(i);
+                positionsInChunk.Add(temp);
+                hashTable.Add(data[i], temp);
+            }
+        }
+    }
+
+
+}
