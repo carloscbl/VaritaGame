0d50c2e647005d9acfbff74d140eaab7eb25da0b
carlos <carloscbl@hotmail.com>
2017-03-02 20:39:48 +0100

    Several improvements and fixes on TerrainGeneration

--- Assets/Terrain/TerrainGeneration/TerrainGeneration.cs
+++ Assets/Terrain/TerrainGeneration/TerrainGeneration.cs
@@ -35,50 +35,12 @@ class TerrainGeneration : MonoBehaviour
     public Gradient coloring;
 
     private Texture2D texture;
-    //public byte [,] GenTerrainNew()
-    //{
-    //    byte[,] data = new byte[resolution, resolution];
-
-    //    NoiseMethod method = Noise.methods[(int)type][dimensions - 1];
-    //    //float stepSize = 1f / resolution;
-
-    //    for (int y = 0; y < resolution; y++)
-    //    {
-    //        //Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
-    //        //Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
-    //        for (int x = 0; x < resolution; x++)
-    //        {
-    //            //Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
-    //            float sample = Noise.Sum(method, new Vector3(x,y,0), frequency, octaves, lacunarity, persistence);
-
-    //            if (type != NoiseMethodType.Value)
-    //            {
-    //                sample = sample * 0.5f + 0.5f;
-    //            }
-    //            if (sample >= 0.3f && sample <= 0.4f)
-    //            {
-    //                data[y, x] = 0;
-    //            }else
-    //            {
-    //                data[y, x] = 1;
-    //            }
-    //            //data[x,y] =
-    //            //texture.SetPixel(x, y, coloring.Evaluate(sample));
-    //        }
-    //    }
-    //    return data;
-    //}
 
     public byte[,] GenTerrainNew()
     {
+
+        float time = Time.realtimeSinceStartup;
         byte[,] data = new byte[2730, 2730];
-
-        
-
-        //Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
-        //Vector3 point10 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
-        //Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
-        //Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
 
         Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
         Vector3 point10 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
@@ -87,49 +49,42 @@ class TerrainGeneration : MonoBehaviour
 
         NoiseMethod method = Noise.methods[(int)type][dimensions - 1];
         float stepSize = 1f / resolution;
+        float stepSizeX = 1f / 500;
         for (int y = 0; y < resolution; y++)
         {
             Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
             Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
             for (int x = 0; x < resolution; x++)
             {
-                Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
-                float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
-
-                if (type != NoiseMethodType.Value)
-                {
-                    sample = sample * 0.5f + 0.5f;
-                }
-                if (/*sample >= 0.3f &&*/ sample <= 0.43f)
+                if (y > 1024)
                 {
                     data[y, x] = 0;
-                }else if (sample > 0.43f && sample < 0.48f)
+                }else
                 {
-                    data[y, x] = 2;
+                    Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
+                    float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+
+                    if (type != NoiseMethodType.Value)
+                    {
+                        sample = sample * 0.5f + 0.5f;
+                    }
+                    if (/*sample >= 0.3f &&*/ sample <= 0.43f)
+                    {
+                        data[y, x] = 0;
+                    }
+                    else if (sample > 0.43f && sample < 0.48f)
+                    {
+                        data[y, x] = 2;
+                    }
+                    else
+                    {
+                        data[y, x] = 1;
+                    }
                 }
-                else
-                {
-                    data[y, x] = 1;
-                }
-                //texture.SetPixel(x, y, coloring.Evaluate(sample));
+                
             }
         }
-        //texture.Apply();
-        //int count = 0;
-        //for (int i = 0; i < 2730; i++)
-        //{
-        //    for (int e = 0; e < 500; e++)
-        //    {
-        //        if (count == 521 || count == 20 || count == 54600 || count == 108990 || count == 165360)
-        //        {
-        //            data[i, e] = 2;
-        //        }else
-        //        {
-        //            data[i, e] = 1;
-        //        }
-        //        count++;
-        //    }
-        //}
+        print(Time.realtimeSinceStartup - time);
         return data;
     }
 
@@ -152,7 +107,7 @@ class TerrainGeneration : MonoBehaviour
         if (transform.hasChanged)
         {
             transform.hasChanged = false;
-            FillTexture();
+            //FillTexture();
         }
     }
 
